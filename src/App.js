faf4fa70e73b4ac8baf7afea16492032fa8df494
fullpistoladev <mateus_henrique2011@outlook.com>
2021-08-09 17:05:38 -0300

    add typewriter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
 import React from "react";
-import {FaTwitter, FaGithub, FaLinkedinIn, FaEnvelope } from "react-icons/fa"
-import {IconContext} from "react-icons"
+import Typewriter from "typewriter-effect";
+import { FaTwitter, FaGithub, FaLinkedinIn, FaEnvelope } from "react-icons/fa";
+import { IconContext } from "react-icons";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <div className="container">
         <div className="image">
-        <img src={process.env.PUBLIC_URL + '/me.jpg'} /> 
+          <img src={process.env.PUBLIC_URL + "/me.jpg"} />
         </div>
-          <h1>I'm a software developer</h1>
-          
-          <IconContext.Provider value={{ style: { hover: 'blue', fontSize: '5vh'}}}>
+        <div className="type">
+          <Typewriter
+            cursorColor="#5D51CF"
+            options={{
+              autoStart: true,
+              loop: true,
+              cursorClassName: "bla",
+              delay: 75,
+            }}
+            onInit={(typewriter) => {
+              typewriter
+
+                .typeString("My name is Mateus Henrique")
+                .pauseFor(1500)
+                .deleteAll()
+                .typeString("I'm a Software Developer")
+                .pauseFor(1500)
+                .deleteAll()
+                .typeString("I'm a computer science student")
+                .pauseFor(1500)
+                .start();
+            }}
+          />
+        </div>
+
+        <IconContext.Provider
+          value={{ style: { hover: "blue", fontSize: "5vh" } }}
+        >
           <nav>
-            <a href="https://github.com/fullpistoladev" target="_blank" ><FaGithub /></a>
-            <a href="https://www.linkedin.com/in/mathenrifs" target="_blank"><FaLinkedinIn /></a>
-            <a href="https://twitter.com/Fullpistoladev" target="_blank"><FaTwitter /></a>
-            <a><FaEnvelope /></a>
-       </nav>
-      </IconContext.Provider>
-            {/* <li><button><i className="btn fab fa-github-square"></i></button></li>
+            <a href="https://github.com/fullpistoladev" target="_blank">
+              <FaGithub />
+            </a>
+            <a href="https://www.linkedin.com/in/mathenrifs" target="_blank">
+              <FaLinkedinIn />
+            </a>
+            <a href="https://twitter.com/Fullpistoladev" target="_blank">
+              <FaTwitter />
+            </a>
+            <a>
+              <FaEnvelope />
+            </a>
+          </nav>
+        </IconContext.Provider>
+        {/* <li><button><i className="btn fab fa-github-square"></i></button></li>
             <li><i className="fab fa-linkedin"></i></li>
             <li><i className="fab fa-twitter-square"></i></li>
             <li><i className="fas fa-envelope-square"></i></li> */}
-         
       </div>
     </div>
   );
