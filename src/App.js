25436753bebd72440220ea8a0ec831feafd58ab4
fullpistoladev <mateus_henrique2011@outlook.com>
2021-08-09 20:31:17 -0300

    making resposive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Typewriter from "typewriter-effect";
 import { FaTwitter, FaGithub, FaLinkedinIn, FaEnvelope } from "react-icons/fa";
 import { IconContext } from "react-icons";
@@ -6,55 +6,76 @@ import { IconContext } from "react-icons";
 import "./App.css";
 
 function App() {
+
+  const [email, setEmail] = useState(false);
+
   return (
     <div className="app">
       <div className="container">
         <div className="image">
-          <img src={process.env.PUBLIC_URL + "/me.jpg"} />
+          <img src={process.env.PUBLIC_URL + "/me.jpg"} alt='me'/>
         </div>
         <div className="type">
-          <Typewriter
-            cursorColor="#5D51CF"
-            options={{
-              autoStart: true,
-              loop: true,
-              cursorClassName: "bla",
-              delay: 75,
-            }}
-            onInit={(typewriter) => {
-              typewriter
+            <Typewriter
+              options={{
+                cursorClassName: "cursor",
+                autoStart: true,
+                loop: true,
+                delay: 75,
+              }}
+              onInit={(typewriter) => {
+                typewriter
 
-                .typeString("My name is Mateus Henrique")
-                .pauseFor(1500)
-                .deleteAll()
-                .typeString("I'm a Software Developer")
-                .pauseFor(1500)
-                .deleteAll()
-                .typeString("I'm a computer science student")
-                .pauseFor(1500)
-                .start();
-            }}
-          />
+                  .typeString("My name is Mateus Henrique")
+                  .pauseFor(1500)
+                  .deleteAll()
+                  .typeString("I'm a Software Developer")
+                  .pauseFor(1500)
+                  .deleteAll()
+                  .typeString("I'm a computer science student")
+                  .pauseFor(1500)
+                  .start();
+              }}
+            />
         </div>
 
         <IconContext.Provider
           value={{ style: { hover: "blue", fontSize: "5vh" } }}
         >
           <nav>
-            <a href="https://github.com/fullpistoladev" target="_blank">
+            <a href="https://github.com/fullpistoladev" target="_blank" rel="noreferrer">
               <FaGithub />
             </a>
-            <a href="https://www.linkedin.com/in/mathenrifs" target="_blank">
+            <a href="https://www.linkedin.com/in/mathenrifs" target="_blank" rel="noreferrer">
               <FaLinkedinIn />
             </a>
-            <a href="https://twitter.com/Fullpistoladev" target="_blank">
+            <a href="https://twitter.com/Fullpistoladev" target="_blank" rel="noreferrer">
               <FaTwitter />
             </a>
-            <a>
+            <button onClick={() => setEmail(!email)}>
               <FaEnvelope />
-            </a>
+            </button>
           </nav>
         </IconContext.Provider>
+
+          {email && <div className="type email">
+            <Typewriter
+              options={{
+                autoStart: true,
+                loop: false,
+                cursorClassName: "cursor",
+                delay: 75,
+              }}
+              onInit={(typewriter) => {
+                typewriter
+
+                .typeString("mathenrique@ufpi.edu.br")
+                .start();
+              }}
+            />
+        </div>
+        }
+       
       </div>
     </div>
   );
